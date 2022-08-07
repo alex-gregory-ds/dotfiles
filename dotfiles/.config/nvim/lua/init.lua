8a5298fa225190821150925a2db1903eaf720b9d
Alex Gregory <alex.gregory1@outlook.com>
2022-08-07 13:47:21 +0100

    Add git signs package to replace git gutter.

--- dotfiles/.config/nvim/lua/init.lua
+++ dotfiles/.config/nvim/lua/init.lua
@@ -1,5 +1,6 @@
 require('colourschemes')     -- Colourscheme configurations
 require('mappings')          -- Keyboard mappings
+require('gitsigns-config')   -- Config for gitsigns
 require('nvim-tree-config')  -- Config for nvim-tree
 require('plugins')           -- Package install scripts
 require('settings')          -- General settings
--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -1,10 +1,21 @@
 return require('packer').startup(function(use)
+
+    -- Package manager
     use 'wbthomason/packer.nvim'
 
+    -- Colourscheme
     use "EdenEast/nightfox.nvim" -- Colourscheme
 
+    -- File explorer
     use {
         'kyazdani42/nvim-tree.lua',
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
+
+    -- Git signs in the gutter
+    use {
+        'lewis6991/gitsigns.nvim',
+        -- tag = 'release' -- To use the latest release
+    }
+
 end)
