3315cd49309f577247794c238d46cff8b1b22eb0
Alex Gregory <alex.gregory1@outlook.com>
2022-09-11 14:40:34 +0100

    Move general settings into init.lua

--- dotfiles/.config/nvim/lua/init.lua
+++ dotfiles/.config/nvim/lua/init.lua
@@ -2,5 +2,17 @@ require('colourschemes')     -- Colourscheme configurations
 require('mappings')          -- Keyboard mappings
 require('gitsigns-config')   -- Config for gitsigns
 require('plugins')           -- Package install scripts
-require('settings')          -- General settings
+
+vim.opt.wrap = false            -- Turn off textwrap
+vim.opt.mouse = a               -- Enable mouse clicking
+vim.opt.number = true           -- Line numbers
+vim.opt.tabstop = 4             -- Tab width
+vim.opt.expandtab = true        -- Expand tabs into spaces
+vim.opt.shiftwidth = 4          -- Indents will have a width of 4
+vim.opt.relativenumber = true   -- Relative line numbers
+vim.opt.scrolloff = 5           -- Start scrolling before final line
+
+-- Map "jk" and "kj" to "<Esc>"
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true })
 
