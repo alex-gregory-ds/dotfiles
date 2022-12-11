48ee25b77c2bdd06572ffb50a351ab9300d166ec
Alex Gregory <alex.gregory1@outlook.com>
2022-12-11 13:22:20 +0000

    Remove LSP and autocompletion plugins

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -33,15 +33,15 @@
     use 'tpope/vim-commentary'
 
     -- Language server protocal config
-    use 'neovim/nvim-lspconfig'
+    -- use 'neovim/nvim-lspconfig'
 
     -- Packages for autocompletion
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    -- use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
 
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    -- use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
 
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    -- use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
 
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    -- use 'L3MON4D3/LuaSnip' -- Snippets plugin
 
 end)
