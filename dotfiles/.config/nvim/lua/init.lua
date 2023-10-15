d1539fc0b69371c316cd90a4d1f7524a07a903cf
Alex Gregory <alex.gregory1@outlook.com>
2023-10-15 16:50:46 +0100

    Change color scheme to sorbet
    Add settings to make visual mode highlighting better and diffs better.

--- dotfiles/.config/nvim/lua/init.lua
+++ dotfiles/.config/nvim/lua/init.lua
@@ -9,7 +9,7 @@ vim.opt.scrolloff = 5           -- Start scrolling before final line
 vim.opt.cursorline = true       -- Highlight current line
 vim.opt.indentexpr = ""
 
-vim.api.nvim_command "colorscheme habamaxx"
+vim.api.nvim_command "colorscheme sorbet"
 
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
@@ -26,3 +26,11 @@ vim.opt.path = vim.opt.path + "**"
 vim.opt.wildmenu = true
 vim.opt.wildignore = vim.opt.wildignore + "**/.git/*"
 
+-- Reverse colors in visual mode
+vim.api.nvim_command('hi Visual ctermfg=NONE ctermbg=NONE cterm=reverse')
+
+-- Improve diff colors
+vim.api.nvim_command('hi DiffAdd ctermfg=NONE ctermbg=17 cterm=NONE')
+vim.api.nvim_command('hi DiffText ctermfg=NONE ctermbg=53 cterm=NONE')
+vim.api.nvim_command('hi DiffChange ctermfg=NONE ctermbg=236 cterm=NONE')
+vim.api.nvim_command('hi DiffDelete ctermfg=137 ctermbg=NONE cterm=NONE')
