47bf8eb715c9b615c6dd89e63d9a3d60773ad2f9
Alex Gregory <alex.gregory1@outlook.com>
2025-02-08 19:21:51 +0000

    Remove colorscheme modifications in init.lua

--- dotfiles/.config/nvim/lua/init.lua
+++ dotfiles/.config/nvim/lua/init.lua
@@ -27,15 +27,6 @@ vim.opt.path = vim.opt.path + "**"
 vim.opt.wildmenu = true
 vim.opt.wildignore = vim.opt.wildignore + "**/.git/*"
 
--- Reverse colors in visual mode
--- vim.api.nvim_command('hi Visual ctermfg=NONE ctermbg=NONE cterm=reverse guifg=NONE guibg=NONE gui=reverse cterm=reverse')
-
--- Improve diff colors
--- vim.api.nvim_command('hi DiffAdd ctermfg=NONE ctermbg=17 cterm=NONE')
--- vim.api.nvim_command('hi DiffText ctermfg=NONE ctermbg=53 cterm=NONE')
--- vim.api.nvim_command('hi DiffChange ctermfg=NONE ctermbg=236 cterm=NONE')
--- vim.api.nvim_command('hi DiffDelete ctermfg=137 ctermbg=NONE cterm=NONE')
-
 -- Quickly open a todo list
 map('n', '<leader>t', ':e ~/notes/todo.md <cr>', opts)
 map('n', '<leader>T', ':e ~/notes/ <cr>', opts)
