861114c24ef547d5199b992858c337329bcf940d
Alex Gregory <alex.gregory1@outlook.com>
2026-03-31 15:47:31 +0100

    Add install command for MacOS

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ SOURCE_DIR="${HOME}/dotfiles/dotfiles/."
 
 cp --recursive --symbolic-link --force ${SOURCE_DIR} ${HOME}
 
+# This is the equivalent to the command above but for MacOS
+# cp -R -P -f ${SOURCE_DIR} ${HOME}
+
